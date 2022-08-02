774f9b708f0152759261e1ecafd75ea725ae2cdf
fitnesswithrohit <rohit.sharma.e21@nsut.ac.in>
2022-08-02 15:19:41 +0530

    chat app

---
+++ Node Server/index.js
@@ -0,0 +1,17 @@
+// Node server which will handle socket io connection.
+
+const io = require('socket.io')(8000)
+
+const users = {};
+
+io.on('connection', socket =>{
+  socket.on('new-user-joined', name =>{
+    console.log("New user", name)
+    user[socket.id] = name;
+    socket.broadcast.emit('user-joined', name);
+  });
+
+  socket.on('send', message =>{
+    socket.broadcast.emit('receive', {message: message, name: user[socket.id]})
+  })
+});
